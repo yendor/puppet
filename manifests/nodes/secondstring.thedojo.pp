ccd93c921b38145557d4e86775c99cbea190fe85
Rodney Amato <rodney.amato@puppetmaster.thedojo>
2010-03-01 09:52:28 +1100

    Renamed some nodes to have proper fqdns

--- manifests/nodes/secondstring.thedojo.pp
+++ manifests/nodes/secondstring.thedojo.pp
@@ -1,8 +1,11 @@
-node "test" {
+node "secondstring.thedojo" {
     $mirror="http://ftp.au.debian.org/debian"
 	include common
 	include edge-remote-logging
 
+	$isPlesk=true
 	include fail2ban
+
+	include user::unixadmins
 }
 
--- manifests/nodes/test.thedojo.pp
+++ manifests/nodes/test.thedojo.pp
@@ -1,9 +1,9 @@
-node "secondstring" {
+node "test.thedojo" {
     $mirror="http://ftp.au.debian.org/debian"
 	include common
 	include edge-remote-logging
 
-	$isPlesk=true
 	include fail2ban
+	include user::unixadmins
 }
 
