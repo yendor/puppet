343cb25fd1c6db5f09ac9fed7f9c66f25f2acdbe
Rodney Amato <rodney.amato@puppetmaster.thedojo>
2010-03-01 09:53:29 +1100

    Added the unixadmins class

--- manifests/nodes/puppetmater.thedojo.pp
+++ manifests/nodes/puppetmater.thedojo.pp
@@ -1,7 +1,13 @@
 node "puppetmaster.thedojo" {
+	# Setup the sources.list
     $mirror="http://ftp.au.debian.org/debian"
+	$includeBackports=true
+	include sources-list, backports-keyring
+
 	include common
 	include midpoint-remote-logging
+
+	include user::unixadmins
 }
 
 
