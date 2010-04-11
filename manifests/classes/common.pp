7142d51a48a6bf1d04a23fea95a4bc0ea5fff44d
Rodney Amato <rodnet@gmail.com>
2010-04-11 13:01:33 +1000

    Add htop to the base soe

--- manifests/classes/common.pp
+++ manifests/classes/common.pp
@@ -1,6 +1,6 @@
 class common {
 #	include shared-hosts
-    include vim, bash, ssh, less, rsync, apticron, logcheck, mail::aliases, clear-apt-cache, avahi, sudo
+    include vim, bash, ssh, less, rsync, apticron, logcheck, mail::aliases, clear-apt-cache, avahi, sudo, htop
 
 	$includeBackports=true
 	include sources-list, backports-keyring
