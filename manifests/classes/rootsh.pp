4f0b2c57652e15d4b4b6fca5bb3d6ddc5b1b74f3
Rodney Amato <rodnet@gmail.com>
2010-05-04 12:05:15 +1000

    Include rootsh on the test machine

--- manifests/classes/rootsh.pp
+++ manifests/classes/rootsh.pp
@@ -1,13 +1,5 @@
 class rootsh {
     package { "rootsh":
         ensure => "present",
-        require => File["/etc/apt/sources.list.d/interspire_custom.list"]
     }
-
-    file { "/etc/apt/sources.list.d/interspire_custom.list":
-        owner => root,
-        group => root,
-        mode => 0644,
-    }
-
 }
