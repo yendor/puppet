9aa808e7e859e409a03ee4229a69dfe6f6d9db07
Rodney Amato <rodnet@gmail.com>
2010-05-07 23:07:45 +1000

    Remove the target property

--- modules/puppet-shells/lib/puppet/type/shells.rb
+++ modules/puppet-shells/lib/puppet/type/shells.rb
@@ -1,5 +1,3 @@
-require 'puppet/provider/parsedfile'
-
 Puppet::Type.newtype(:shells) do
   @doc = "Manage shells in /etc/shells. For example::
 
@@ -15,16 +13,5 @@ Puppet::Type.newtype(:shells) do
   newparam(:shell, :namevar => true) do
     desc "The shell to manage"
     isnamevar
-  end
-
-  newproperty(:target) do
-    desc "Location of the shells file"
-    defaultto {
-      if @resource.class.defaultprovider.ancestors.include?(Puppet::Provider::Parsefile)
-        @resource.class.defaultprovider.default_target
-      else
-        nil
-      end
-    }
   end
 end
