a972d855cc2e9341d36d49c020e015b8df3cab4d
Rodney Amato <rodnet@gmail.com>
2010-05-07 22:21:45 +1000

    Added the shells type

---
+++ modules/puppet-shells/lib/puppet/type/shells.rb
@@ -0,0 +1,30 @@
+module Puppet
+  newtype(:shells) do
+      @doc = "Manage shells in /etc/shells. For example::
+
+      shells {\"/bin/bash\":
+        ensure => present
+      }
+
+      There is also an optional target attribute if your
+      shells file is located elsewhere."
+
+      ensurable
+
+      newparam(:shell, :namevar => true) do
+        desc "The shell to manage"
+        isnamevar
+      end
+
+      newproperty(:target) do
+        desc "Location of the shells file"
+        defaultto {
+          if @resource.class.defaultprovider.ancestors.include? (Puppet::Provider::Parsefile)
+            @resource.class.defaultprovider.default_target
+          else
+            nil
+          end
+        }
+      end
+  end
+end
