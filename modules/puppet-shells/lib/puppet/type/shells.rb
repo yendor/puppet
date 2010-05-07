be9375402299cd84095a9c8f0de174c29428b488
Rodney Amato <rodnet@gmail.com>
2010-05-07 22:32:17 +1000

    Switch to the newer style definition of the module type

--- modules/puppet-shells/lib/puppet/type/shells.rb
+++ modules/puppet-shells/lib/puppet/type/shells.rb
@@ -1,30 +1,28 @@
-module Puppet
-  newtype(:shells) do
-      @doc = "Manage shells in /etc/shells. For example::
+Puppet::Type.newtype(:shells) do
+  @doc = "Manage shells in /etc/shells. For example::
 
-      shells {\"/bin/bash\":
-        ensure => present
-      }
+  shells {\"/bin/bash\":
+    ensure => present
+  }
 
-      There is also an optional target attribute if your
-      shells file is located elsewhere."
+  There is also an optional target attribute if your
+  shells file is located elsewhere."
 
-      ensurable
+  ensurable
 
-      newparam(:shell, :namevar => true) do
-        desc "The shell to manage"
-        isnamevar
+  newparam(:shell, :namevar => true) do
+    desc "The shell to manage"
+    isnamevar
+  end
+
+  newproperty(:target) do
+    desc "Location of the shells file"
+    defaultto {
+      if @resource.class.defaultprovider.ancestors.include?(Puppet::Provider::Parsefile)
+        @resource.class.defaultprovider.default_target
+      else
+        nil
       end
-
-      newproperty(:target) do
-        desc "Location of the shells file"
-        defaultto {
-          if @resource.class.defaultprovider.ancestors.include?(Puppet::Provider::Parsefile)
-            @resource.class.defaultprovider.default_target
-          else
-            nil
-          end
-        }
-      end
+    }
   end
 end
