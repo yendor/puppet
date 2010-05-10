77375b7deb9a214b4ec25e0edc9d2134ce5ec344
Rodney Amato <rodnet@gmail.com>
2010-05-10 21:53:47 +1000

    Renamed the provider to properly reflect the dns backend

--- modules/puppet-dns/lib/puppet/provider/dnsrecord/tinydns.rb
+++ modules/puppet-dns/lib/puppet/provider/dnsrecord/tinydns.rb
@@ -1,9 +1,9 @@
 require 'puppet/provider/parsedfile'
 datafile = "/etc/tinydns/root/data"
 
-Puppet::Type.type(:shells).provide(:parsed, :parent => Puppet::Provider::ParsedFile, :default_target => datafile, :filetype => :flat) do
+Puppet::Type.type(:dnsrecord).provide(:tinydns, :parent => Puppet::Provider::ParsedFile, :default_target => datafile, :filetype => :flat) do
 
-    desc "The shells provider that uses the ParsedFile class"
+    desc "The tinydns data file"
 
     text_line :comment, :match => /^#/;
     text_line :blank, :match => /^\s*$/;
