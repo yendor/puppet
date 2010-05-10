88316117b8c5924402cbb625e32fccf2f5538097
Rodney Amato <rodnet@gmail.com>
2010-05-10 21:49:10 +1000

    Make sure we ensure the record is present

--- manifests/nodes/puppetmaster.thedojo.pp
+++ manifests/nodes/puppetmaster.thedojo.pp
@@ -1,10 +1,10 @@
 node "puppetmaster.thedojo" {
     $mirror="http://ftp.au.debian.org/debian"
 	include common
-	include midpoint-remote-logging
 	include git
 
     dnsrecord {"login.thedojo":
+        ensure => "present",
         type => "a",
         value => "192.168.1.12",
         ttl => 300,
