bbb477aa662469a5cb4623f75bfdadcb6cf2d387
Rodney Amato <rodnet@gmail.com>
2010-05-18 00:12:58 +1000

    Testing some changes to get the tinydns module going

--- modules/dnsrecord/lib/puppet/provider/dnsrecord/parsed.rb
+++ modules/dnsrecord/lib/puppet/provider/dnsrecord/parsed.rb
@@ -9,8 +9,8 @@ Puppet::Type.type(:dnsrecord).provide(:parsed, :parent => Puppet::Provider::Pars
     text_line :blank, :match => /^\s*$/;
 
     record_line self.name,
-        :fields => %w{type name value ttl}
-        do |line|
+        :fields => %w{type name value ttl},
+        :separator => ":" do |line|
           hash = {}
 
           if line.sub!(/(.)([^:]*):([^:]*):([^:]*):([^:]*):([^:]*):/)
