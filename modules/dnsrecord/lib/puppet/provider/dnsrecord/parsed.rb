f5d0e0cb87ce76ac3c17538eae60db701d455400
Rodney Amato <rodnet@gmail.com>
2010-05-20 22:56:50 +1000

    Debugging the tinydns module

--- modules/dnsrecord/lib/puppet/provider/dnsrecord/parsed.rb
+++ modules/dnsrecord/lib/puppet/provider/dnsrecord/parsed.rb
@@ -17,7 +17,7 @@ Puppet::Type.type(:dnsrecord).provide(:parsed, :parent => Puppet::Provider::Pars
           line =~ /^(.)/
 
           case $1
-            when "="
+            when "=", "+"
               parts = line[1, line.length].split(':')
               parts.each do |part|
                 puts part
@@ -33,7 +33,7 @@ Puppet::Type.type(:dnsrecord).provide(:parsed, :parent => Puppet::Provider::Pars
       str = hash[:type]
 
       case hash[:type]
-        when "="
+        when "=", "+"
           str += [hash[:name], hash[:value], "", hash[:ttl], ""].join(":").sub(/:+$/, "")
       end
     end
