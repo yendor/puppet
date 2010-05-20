e074fbce09073a10cad2d69604073615f01126aa
Rodney Amato <rodnet@gmail.com>
2010-05-20 22:22:05 +1000

    Testing the validation

--- modules/dnsrecord/lib/puppet/provider/dnsrecord/parsed.rb
+++ modules/dnsrecord/lib/puppet/provider/dnsrecord/parsed.rb
@@ -23,7 +23,7 @@ Puppet::Type.type(:dnsrecord).provide(:parsed, :parent => Puppet::Provider::Pars
           # +puppet.interspire:174.37.171.132
           # &interspire.corp::whitebook.interspire:::
 
-          if line =~ (/^(.)([^:]*):([^:]*):([^:]*):([^:]*):([^:]*)\s*$/)
+          if line =~ (/^(.)([^:]*):([^:]*):?([^:]*):?([^:]*):?([^:]*)\s*$/)
             hash[:type] = $1
             hash[:name] = $2
             hash[:value] = $3
