98f09948b022f3fc40c7e44c6cf50714360271c3
Rodney Amato <rodnet@gmail.com>
2010-05-21 21:37:22 +1000

    Tweak the record types so the name is a text field to allow multiple records of different types for the same fqdn

--- modules/tinydns/lib/puppet/provider/dnsrecord/parsed.rb
+++ modules/tinydns/lib/puppet/provider/dnsrecord/parsed.rb
@@ -9,8 +9,8 @@ Puppet::Type.type(:dnsrecord).provide(:parsed, :parent => Puppet::Provider::Pars
     text_line :blank, :match => /^\s*$/;
 
     record_line self.name,
-        :fields => %w{type name value ttl stamp location priority host},
-        :optional => %w{ttl stamp location priority host},
+        :fields => %w{type name fqdn value ttl stamp location priority host ip},
+        :optional => %w{ttl stamp location priority host ip value},
         :separator => ":" do |line|
           hash = {}
           empty = {}
