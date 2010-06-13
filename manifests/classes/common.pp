f3b178ee2321f46465259860965f74e65dd63f2c
Rodney Amato <rodnet@gmail.com>
2010-06-13 21:56:26 +1000

    Indenting change

--- manifests/classes/common.pp
+++ manifests/classes/common.pp
@@ -1,23 +1,23 @@
 class common {
-  include vim, bash, ssh, less, rsync, apticron, logcheck, mail::aliases, clear-apt-cache, avahi, sudo, htop, bzip2
-  include puppet
-  include postfix::standard
+    include vim, bash, ssh, less, rsync, apticron, logcheck, mail::aliases, clear-apt-cache, avahi, sudo, htop, bzip2
+    include puppet
+    include postfix::standard
 
-  include rodney-home
+    include rodney-home
 
-	$includeBackports=true
-	include sources-list, backports-keyring, syslog-ng
+    $includeBackports=true
+    include sources-list, backports-keyring, syslog-ng
 
-	include user::unixadmins
+    include user::unixadmins
 
-	include tinydns::setup
+    include tinydns::setup
 
-  @@dnsrecord { "forward and reverse dns for $fqdn":
-    ensure => "present",
-    type => "=",
-    fqdn => "$fqdn",
-    ipaddr => "$ipaddress",
-    ttl => 300,
-    notify => Exec["rebuild-tinydns-data"]
-  }
+    @@dnsrecord { "forward and reverse dns for $fqdn":
+        ensure => "present",
+        type => "=",
+        fqdn => "$fqdn",
+        ipaddr => "$ipaddress",
+        ttl => 300,
+        notify => Exec["rebuild-tinydns-data"]
+    }
 }
