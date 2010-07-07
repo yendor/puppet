d42a53fc60d7468c647aca06d661afc18a41664c
Rodney Amato <rodnet@gmail.com>
2010-07-07 14:24:09 +1000

    Added mysql user

--- manifests/nodes/test.thedojo.pp
+++ manifests/nodes/test.thedojo.pp
@@ -27,5 +27,19 @@ node "test.thedojo" {
 	shells { "/bin/sync":
 	    ensure => present
 	}
+
+
+	include mysql::server
+	mysql::user { "Allow access for rodney.amato":
+	    ensure => present,
+	    name => "rodney.amato@localhost",
+	    password_hash => "*514699B1AEE5B468012A8156585A02A6E843B277",
+	}
+
+	mysql::rights{"Allow DBA access for rodney.amato":
+      ensure   => present,
+      user     => "rodney.amato",
+      host      => "localhost"
+    }
 }
 
