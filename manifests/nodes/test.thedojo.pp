f5ede185d6b56515ddb8fd341ca9f7a871c44594
Rodney Amato <rodnet@gmail.com>
2010-07-07 14:29:39 +1000

    Remove the include of mysql::server

--- manifests/nodes/test.thedojo.pp
+++ manifests/nodes/test.thedojo.pp
@@ -28,16 +28,15 @@ node "test.thedojo" {
 	    ensure => present
 	}
 
-    mysql::user { "Allow access for rodney.amato":
-        ensure => present,
-        name => "rodney.amato@localhost",
-        password_hash => "*514699B1AEE5B468012A8156585A02A6E843B277",
-    }
+    # mysql::user { "Allow access for rodney.amato":
+    #     ensure => present,
+    #     name => "rodney.amato@localhost",
+    #     password_hash => "*514699B1AEE5B468012A8156585A02A6E843B277",
+    # }
 
 	mysql::rights{"Allow DBA access for rodney.amato":
       ensure   => present,
-      user     => "rodney.amato",
-      host      => "localhost"
+      user     => "rodney.amato@localhost",
     }
 }
 
