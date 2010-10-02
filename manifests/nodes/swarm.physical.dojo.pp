c4de0880cd4704882cdfefd49d02edf9973275fe
rodney.amato <rodney.amato@interspire.com>
2010-10-02 16:56:27 +1000

    Include the right class

--- manifests/nodes/swarm.physical.dojo.pp
+++ manifests/nodes/swarm.physical.dojo.pp
@@ -2,7 +2,7 @@ node "swarm.physical.dojo" {
     $mirror="http://ftp.au.debian.org/debian"
     include common
 
-    include virtual-server
+    include virtual-machine-server
 
      virtual-machine::kvm { "puppet":
         ensure => present,
