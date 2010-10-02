070e00d48f76dcfc7e713911ed95cc878dea1375
rodney.amato <rodney.amato@interspire.com>
2010-10-02 16:26:45 +1000

    Disable the commit notification for now since the gem doesnt seem to be available to install

--- manifests/classes/git.pp
+++ manifests/classes/git.pp
@@ -3,30 +3,30 @@ class git {
     	ensure => installed
     }
 
-    package { "git-commit-notifier":
-    	ensure => installed,
-    	provider => "gem",
-    	require => [Package["libhpricot-ruby"], Package["libmocha-ruby"], Package["libdifflcs-ruby"], Package["git-core"]]
-    }
-
-    package { "libhpricot-ruby":
-    	ensure => present,
-    	require => [Package["ruby1.8-dev"], Package["build-essential"]]
-    }
-
-    package { "libmocha-ruby":
-    	ensure => present,
-    }
-
-    package { "libdifflcs-ruby":
-    	ensure => present,
-    }
-
-    package { "ruby1.8-dev":
-    	ensure => installed
-    }
-
-    package { "build-essential":
-    	ensure => installed
-    }
+    # package { "git-commit-notifier":
+    #   ensure => installed,
+    #   provider => "gem",
+    #   require => [Package["libhpricot-ruby"], Package["libmocha-ruby"], Package["libdifflcs-ruby"], Package["git-core"]]
+    # }
+    #
+    # package { "libhpricot-ruby":
+    #   ensure => present,
+    #   require => [Package["ruby1.8-dev"], Package["build-essential"]]
+    # }
+    #
+    # package { "libmocha-ruby":
+    #   ensure => present,
+    # }
+    #
+    # package { "libdifflcs-ruby":
+    #   ensure => present,
+    # }
+    #
+    # package { "ruby1.8-dev":
+    #   ensure => installed
+    # }
+    #
+    # package { "build-essential":
+    #   ensure => installed
+    # }
 }
