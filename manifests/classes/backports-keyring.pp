f5bb49db51eebb82857053320e7688998fbdabca
rodney.amato <rodney.amato@interspire.com>
2010-10-02 16:43:10 +1000

    Remove the backports keyring since its not necessary anymore

--- manifests/classes/backports-keyring.pp
+++
@@ -1,9 +0,0 @@
-class backports-keyring {
-	include wget
-	exec { "install-backports-key" :
-		command => "/usr/bin/wget -O - http://backports.org/debian/archive.key | /usr/bin/apt-key add -",
-		require => Package["wget"],
-		unless => "/usr/bin/apt-key list | /bin/grep -q 'Backports.org Archive Key <ftp-master@backports.org>'",
-	}
-}
-
