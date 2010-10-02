429ab5747cd47ca48ff48c9860b3e691bb097b90
rodney.amato <rodney.amato@interspire.com>
2010-10-02 16:03:46 +1000

    Pad out the automation of the setup of tinydns and dnscache

--- modules/tinydns/manifests/setup.pp
+++ modules/tinydns/manifests/setup.pp
@@ -1,15 +1,52 @@
 class tinydns::setup {
-  exec { "rebuild-tinydns-data":
-    cwd => "/etc/tinydns/root",
-    command => "/usr/bin/make",
-    refreshonly => true
-  }
+    exec { "rebuild-tinydns-data":
+        cwd => "/etc/tinydns/root",
+        command => "/usr/bin/make",
+        refreshonly => true,
+        requires => Package["djbdns"]
+    }
 
-  package { "djbdns":
-    ensure => "present"
-  }
+    exec { "tinydns-setup":
+        command => "/usr/bin/tinydns-conf tinydns dnslog /etc/tinydns 127.0.0.1",
+        creates => "/etc/tinydns",
+        requires => Package["djbdns"]
+    }
 
-  package { "make":
-    ensure => "present"
-  }
+    exec { "dnscache-setup":
+        command => "/usr/bin/dnscache-conf dnscache dnslog /etc/dnscache $ipaddress",
+        creates => "/etc/dnscache",
+        requires => Package["djbdns"]
+    }
+
+    user { "tinydns":
+        ensure => present,
+        comment => "Tinydns User",
+        home => "/dev/null",
+        shell => "/bin/false",
+        uid => 100,
+    }
+
+    user { "dnscache":
+        ensure => present,
+        comment => "Tinydns User",
+        home => "/dev/null",
+        shell => "/bin/false",
+        uid => 101,
+    }
+
+    user { "dnslog":
+        ensure => present,
+        comment => "Tinydns User",
+        home => "/dev/null",
+        shell => "/bin/false",
+        uid => 102,
+    }
+
+    package { "djbdns":
+        ensure => "present"
+    }
+
+    package { "make":
+        ensure => "present"
+    }
 }
