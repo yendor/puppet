73c1441cd7a8fc38987f8c5289c3fb8cda1c7838
rodney.amato <rodney.amato@interspire.com>
2010-11-01 15:54:37 +1100

    Disable transmission for now

--- manifests/classes/transmission.pp
+++ manifests/classes/transmission.pp
@@ -1,20 +1,20 @@
 class transmission {
     package { "transmission-daemon":
-        ensure => "1.77-1~bpo50+2"
+        ensure => "absent"
     }
 
-    service { "transmission-daemon":
-        ensure => running,
-        hasrestart => true,
-        hasstatus => true
-    }
-
-    file { "/etc/transmission-daemon/settings.json":
-        source => "puppet:///files/transmission/settings.json",
-        owner => "root",
-        group => "root",
-        mode => "0644",
-        notify => Service["transmission-daemon"],
-        require => Package["transmission-daemon"]
-    }
+    # service { "transmission-daemon":
+    #     ensure => running,
+    #     hasrestart => true,
+    #     hasstatus => true
+    # }
+    #
+    # file { "/etc/transmission-daemon/settings.json":
+    #     source => "puppet:///files/transmission/settings.json",
+    #     owner => "root",
+    #     group => "root",
+    #     mode => "0644",
+    #     notify => Service["transmission-daemon"],
+    #     require => Package["transmission-daemon"]
+    # }
 }
