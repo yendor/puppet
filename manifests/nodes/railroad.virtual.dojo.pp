ee58e14dfade473b13314cfee75d775dc88c07a5
rodney.amato <rodney.amato@interspire.com>
2010-11-22 18:41:58 +1100

    Add rails

--- manifests/nodes/railroad.virtual.dojo.pp
+++ manifests/nodes/railroad.virtual.dojo.pp
@@ -1,4 +1,13 @@
 node "railroad.virtual.dojo" {
   $mirror="http://ftp.au.debian.org/debian"
   include common
+
+	package { "rubygems":
+		ensure => "1.3.4-1~bpo50+1"
+	}
+
+	package { "rails":
+		ensure => "3.0.3",
+		provider => "gem"
+	}
 }
