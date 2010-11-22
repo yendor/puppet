0289a452488511e344d70b82293465098111d912
rodney.amato <rodney.amato@interspire.com>
2010-11-22 21:18:39 +1100

    Fix a typo

--- manifests/classes/nginx.pp
+++ manifests/classes/nginx.pp
@@ -19,7 +19,7 @@ class nginx {
   define site ( $ensure = 'present' ) {
       case $ensure {
          'present' : {
-            exec { "/bin/ln -s /etc/nginx/sites-available/$name /etc/nginx/sites-enabled/name":
+            exec { "/bin/ln -s /etc/nginx/sites-available/$name /etc/nginx/sites-enabled/$name":
                creates => "/etc/nginx/sites-enabled/$name",
                notify => Exec["reload-nginx"],
                require => [Package["nginx"], File["/etc/nginx/sites-available/$name"]]
