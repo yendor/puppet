e76c9af7cf33372bf8893387285e8828f4cafe3d
rodney.amato <rodney.amato@interspire.com>
2010-11-22 19:39:29 +1100

    Added the rails dependancies

--- manifests/nodes/railroad.virtual.dojo.pp
+++ manifests/nodes/railroad.virtual.dojo.pp
@@ -11,7 +11,91 @@ node "railroad.virtual.dojo" {
 		provider => "gem"
 	}
 	
-	package { "unicorn":
+	package { "i18n":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "activemodel":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "activemodel":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "rack":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "rack-test":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "rack-mount":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "tzinfo":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "abstract":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "erubis":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "actionpack":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "arel":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "activerecord":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "activeresource":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "mime-types":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "polyglot":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "treetop":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "mail":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "actionmailer":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "rake":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "thor":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "railties":
+		ensure => "present",
+		provider => "gem"
+	}
+	package { "bundler":
 		ensure => "present",
 		provider => "gem"
 	}
