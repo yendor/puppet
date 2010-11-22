3859a598c62d1ffb6c4a4b01c294730d537c6052
rodney.amato <rodney.amato@interspire.com>
2010-11-22 22:18:37 +1100

    Added dependencies for the gem installs

--- manifests/classes/railsserver.pp
+++ manifests/classes/railsserver.pp
@@ -4,7 +4,8 @@ class railsserver {
 	}
 	package { "rails":
 		ensure => "3.0.3",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "ruby1.8-dev":
 		ensure => "present"
@@ -16,86 +17,107 @@ class railsserver {
 	}
 	package { "i18n":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "activemodel":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "rack":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "rack-test":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "rack-mount":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "tzinfo":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "abstract":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "erubis":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "actionpack":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "arel":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "activerecord":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "activeresource":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "mime-types":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "polyglot":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "treetop":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "mail":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "actionmailer":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "rake":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "thor":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "railties":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 	package { "bundler":
 		ensure => "present",
-		provider => "gem"
+		provider => "gem",
+		require => Package["rubygems1.8"]
 	}
 }
