54815a50213f36754a8cf5f5ecd71c9077f93160
rodney.amato <rodney.amato@interspire.com>
2010-12-04 14:01:01 +1100

    Added some version dependencies to the virtual-machine-server

--- manifests/classes/virtual-machine-server.pp
+++ manifests/classes/virtual-machine-server.pp
@@ -6,14 +6,24 @@ class virtual-machine-server {
         ensure => present
     }
     package { "virtinst":
-        ensure => present
-    }
-    package { "libvirt-bin":
-        ensure => present
+        ensure => "0.500.3-2~bpo50+1"
     }
     package { "kvm":
-        ensure => present
+        ensure => "1:0.12.5+dfsg-3~bpo50+1"
     }
+    package { "qemu-kvm":
+        ensure => "0.12.5+dfsg-3~bpo50+1"
+    }
+    package { "libvirt-bin":
+        ensure => "0.8.1-2~bpo50+1"
+    }    
+    package { "libvirt0":
+        ensure => "0.8.1-2~bpo50+1"
+    }
+    package { "python-libvirt":
+        ensure => "0.8.1-2~bpo50+1"
+    }
+    
     service { "libvirt-bin":
         ensure => running,
         enable => true,
@@ -24,5 +34,11 @@ class virtual-machine-server {
     file { "/etc/libvirt/qemu/autostart":
         ensure => directory,
         backup => false
+    }
+    
+    file { "/sys/kernel/mm/ksm/run":
+        contents => "1",
+        backup => false,
+        require => Package["linux-image-2.6.32-bpo.5-amd64"]
     }
 }
