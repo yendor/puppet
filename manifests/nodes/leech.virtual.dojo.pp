57c732cc827fc7f6ad25103968eb78e65059b3ae
rodney.amato <rodney.amato@interspire.com>
2011-01-19 15:46:05 +1100

    Comment out a broken rule

--- manifests/nodes/leech.virtual.dojo.pp
+++ manifests/nodes/leech.virtual.dojo.pp
@@ -4,8 +4,8 @@ node "leech.virtual.dojo" {
 
   include transmission
 
-  augeas { "root_partition_noatime":
-      context => "/files/etc/fstab",
-      changes => "set *[file = '/']/opt errors=remount-ro,noatime,nodiratime",
-  }
+  # augeas { "root_partition_noatime":
+  #     context => "/files/etc/fstab",
+  #     changes => "set *[file = '/']/opt errors=remount-ro,noatime,nodiratime",
+  # }
 }
