445555f673b8a1c6f793e19f1ccf3e57ef3a621f
rodney.amato <rodney.amato@interspire.com>
2011-03-11 15:18:22 +1100

    Add the path to test

--- manifests/classes/disk.pp
+++ manifests/classes/disk.pp
@@ -5,7 +5,7 @@ class disk {
 		exec { "live_kernel_scheduler_${name}":
 			command => "/bin/echo ${scheduler} > /sys/block/${name}/queue/scheduler",
 			unless => "/usr/bin/test $(/bin/sed -r 's{(.*\\[|\\].*){{g' /sys/block/${name}/queue/scheduler) = '${scheduler}'",
-			onlyif => "test -f /sys/block/${name}/queue/read_ahead_kb",			
+			onlyif => "/usr/bin/test -f /sys/block/${name}/queue/read_ahead_kb",			
 		}
 		
 		augeas { "boot_kernel_scheduler_${name}":
