fa1a0886cbabadf6b7c893ca521696eba8285357
rodney.amato <rodney.amato@interspire.com>
2011-03-11 14:13:32 +1100

    Remove the broken kvm::server class and include the testing disk scheduler class

--- manifests/nodes/test.virtual.dojo.pp
+++ manifests/nodes/test.virtual.dojo.pp
@@ -1,8 +1,12 @@
 node "test.virtual.dojo" {
-  $mirror="http://ftp.au.debian.org/debian"
-  $includeBackports=true
-  include common
-  # include puppetmaster
-  
-  include kvm::server
+	$mirror="http://ftp.au.debian.org/debian"
+	$includeBackports=true
+	include common
+	# include puppetmaster
+
+	# include kvm::server
+	disk::scheduler{ "vda":
+		scheduler => "noop"
+	}
+
 }
