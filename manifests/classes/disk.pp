084b39e28d43ebe2296760de080e79b593a72a11
rodney.amato <rodney.amato@interspire.com>
2011-03-11 14:15:38 +1100

    Fix a parse error

---
+++ manifests/classes/disk.pp
@@ -0,0 +1,14 @@
+class disk {
+	define scheduler($scheduler = 'deadline') {
+		exec { "live_kernel_scheduler_${name}":
+			command => "/bin/echo ${scheduler} > /sys/block/${name}/queue/scheduler",
+			unless => "/usr/bin/test $(/bin/sed -r 's{(.*\\[|\\].*){{g' /sys/block/${name}/queue/scheduler) = '${scheduler}'"
+		}
+		
+		augeas { "boot_kernel_scheduler_${name}":
+            context => "/files/etc/grub.conf",
+            changes => "setm title kernel/elevator noop",
+            onlyif => "get title/kernel/elevator != noop"
+        }
+	}
+}
