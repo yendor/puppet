287d88d583b619ca44b19313e7d755210bcab9eb
rodney.amato <rodney.amato@interspire.com>
2011-03-13 00:10:09 +1100

    Add an init script

--- manifests/classes/logstash.pp
+++ manifests/classes/logstash.pp
@@ -23,7 +23,16 @@ class logstash {
 		require => File["/etc/logstash"]
 	}
 	
+	file { "/etc/init.d/logstash":
+		source => "puppet:///files/logstash/logstash.init",
+		owner  => "root",
+		group  => "root",
+		mode   => 0755,
+		backup => false,
+		require => Package["logstash"]
+	}
+	
 	package { "build-essential":
 		ensure => present,
-	}	
+	}
 }
