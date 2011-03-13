a86dfd6fd94ff117f248d3bcf69af929f68632a7
rodney.amato <rodney.amato@interspire.com>
2011-03-13 21:59:43 +1100

    Basic elastic search requirements

---
+++ manifests/classes/elasticsearch.pp
@@ -0,0 +1,31 @@
+class elsaticsearch {
+	include git
+	package { "openjdk-6-jre-headless":
+		ensure => present,
+	}
+	
+	file { "/etc/elasticsearch":
+		ensure => "directory",
+		owner  => "root",
+		group  => "root",
+		mode   => 0755,
+		backup => false,
+	}
+	
+	file { "/etc/elasticsearch/elasticsearch.yaml":
+		source => "puppet:///files/elasticsearch/elasticsearch.yaml",
+		owner  => "root",
+		group  => "root",
+		mode   => 0644,
+		backup => false,
+		require => File["/etc/elasticsearch"]
+	}
+	
+	file { "/etc/init.d/elasticsearch":
+		source => "puppet:///files/elasticsearch/elasticsearch.init",
+		owner  => "root",
+		group  => "root",
+		mode   => 0755,
+		backup => false,
+	}
+}
