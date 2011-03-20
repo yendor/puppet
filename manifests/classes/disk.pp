1eff24c734d2dc8dc5634bf25a77bad3056dfe57
rodney.amato <rodney.amato@interspire.com>
2011-03-20 14:48:34 +1100

    Add squeeze support for setting the scheduler

--- manifests/classes/disk.pp
+++ manifests/classes/disk.pp
@@ -9,9 +9,18 @@ class disk {
 		}
 		
 		augeas { "boot_kernel_scheduler_${name}":
-			context => "/files/boot/grub/menu.lst",
-			changes => "set debian/defoptions elevator=${scheduler}",
-			onlyif  => "get debian/defoptions != elevator=${scheduler}",
+			context => $lsbdistcodename ? {
+				lenny => "/files/boot/grub/menu.lst",
+				squeeze => "/files/etc/default/grub",
+			},
+			changes => $lsbdistcodename ? {
+				lenny => "set debian/defoptions elevator=${scheduler}",
+				squeeze => "set GRUB_CMDLINE_LINUX_DEFAULT quiet scheduler=noop",
+			},
+			onlyif => $lsbdistcodename ? {
+				lenny => "get debian/defoptions != elevator=${scheduler}",
+				squeeze => "get GRUB_CMDLINE_LINUX_DEFAULT != quiet scheduler=noop",
+			},
 			notify => Exec["update-grub"],
 		}
 	}
