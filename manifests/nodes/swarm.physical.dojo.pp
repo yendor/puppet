9755df50c9303941b11fcd4346df41f0e63f56f3
rodney.amato <rodney.amato@interspire.com>
2011-03-20 11:43:41 +1100

    Remove the vm again

--- manifests/nodes/swarm.physical.dojo.pp
+++ manifests/nodes/swarm.physical.dojo.pp
@@ -24,7 +24,7 @@ node "swarm.physical.dojo" {
     }
 
     virtual-machine::kvm { "squeeze":
-        ensure => present,
+        ensure => absent,
         extra_args => "auto=true url=http://192.168.1.10/preseed-squeeze.cfg",
 		iso => "http://ftp.au.debian.org/debian/dists/squeeze/main/installer-amd64/"
     }
