228d16313d11f96afce20f34f417ab225cabd409
rodney.amato <rodney.amato@interspire.com>
2011-05-13 21:33:21 +1000

    Setup an ldap server

--- manifests/nodes/swarm.physical.dojo.pp
+++ manifests/nodes/swarm.physical.dojo.pp
@@ -28,4 +28,10 @@ node "swarm.physical.dojo" {
         extra_args => "auto=true url=http://192.168.1.10/preseed-squeeze.cfg",
 		iso => "http://ftp.au.debian.org/debian/dists/squeeze/main/installer-amd64/"
     }
+
+    virtual-machine::kvm { "ldap":
+        ensure => present,
+        extra_args => "auto=true url=http://192.168.1.10/preseed-squeeze.cfg",
+		iso => "http://ftp.au.debian.org/debian/dists/squeeze/main/installer-amd64/"
+    }
 }
