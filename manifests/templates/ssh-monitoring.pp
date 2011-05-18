d600a221ca3b12f5efe18a0e03213b09a6b6f6b0
rodney.amato <rodney.amato@interspire.com>
2011-05-19 09:41:06 +1000

    Pass the instance name through

--- manifests/templates/ssh-monitoring.pp
+++ manifests/templates/ssh-monitoring.pp
@@ -8,7 +8,7 @@ class ssh-monitoring($instance_name, $service_groups='')
         host_name           => $fqdn, 
         service_description => "SSH",
         check_command       => "check_ssh", 
-        tag                 => "nagios_monitored_${instance_name}", 
+        instance_name       => ${instance_name}, 
         servicegroups       => $service_groups,
     }
 	
