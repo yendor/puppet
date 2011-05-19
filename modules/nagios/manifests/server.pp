723d2b124f533774a0362013fef815853dd80545
rodney.amato <rodney.amato@interspire.com>
2011-05-19 22:26:49 +1000

    Add basic auth in for nagios

--- modules/nagios/manifests/server.pp
+++ modules/nagios/manifests/server.pp
@@ -68,6 +68,7 @@ class nagios::server (
 		"/etc/nagios3/conf.d/localhost_nagios2.cfg",
 		"/etc/nagios3/conf.d/hostgroups_nagios2.cfg",
 		"/etc/nagios3/conf.d/services_nagios2.cfg",
+		"/etc/nagios3/conf.d/extinfo_nagios2.cfg",
 		] :
 		ensure => absent,
 		backup => false,
