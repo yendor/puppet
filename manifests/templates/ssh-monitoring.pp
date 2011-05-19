72c91a312dc6512602e6d816325432874ab0be69
rodney.amato <rodney.amato@interspire.com>
2011-05-19 21:30:41 +1000

    Fix the path to the template

--- manifests/templates/ssh-monitoring.pp
+++ manifests/templates/ssh-monitoring.pp
@@ -5,7 +5,7 @@ class ssh-monitoring($instance_name, $service_groups='')
 	class { "nagios::service":
 		name                => "ssh_${fqdn}",
         ensure              => present, 
-        host_name           => $fqdn, 
+        host_name           => $host_name, 
         service_description => "SSH",
         check_command       => "check_ssh", 
         instance_name       => $instance_name, 
--- modules/nagios/manifests/service.pp
+++ modules/nagios/manifests/service.pp
@@ -21,39 +21,11 @@ class nagios::service(
     $depends_on_service = '',
     $depends_on_host_name = ''
 ) {
-
-	@@nagios_service { $name:
-	     ensure                  => $ensure,
-	     service_description     => $use_service_description,
-	     check_command           => $check_command,
-	     use                     => $use,
-	     host_name               => $host_name,
-	     notification_period     => $notification_period,
-	     max_check_attempts      => $max_check_attempts,
-	     retry_check_interval    => $retry_check_interval,
-	     notification_interval   => $notification_interval,
-	     normal_check_interval   => $normal_check_interval,
-	     contact_groups          => $contact_groups,
-	     check_period            => $check_period,
-	     active_checks_enabled   => $active_checks_enabled,
-	     passive_checks_enabled  => $passive_checks_enabled,
-	     check_freshness         => $check_freshness,
-	     freshness_threshold     => $freshness_threshold,
-	     notifications_enabled   => $notifications_enabled,
-	     notify                  => Service["nagios"],
-	     target                  => "/etc/nagios3/conf.d/service-${name}.cfg",
-	     require                 => File["/etc/nagios3/conf.d/service-${name}.cfg"],
-	     servicegroups           => $servicegroups ? {
-	         ''      => undef,
-	         default => $servicegroups,
-	     },
-	     tag                     => "nagios_monitored_${instance_name}", 
-	 }
-	
-	 @@file { "/etc/nagios3/conf.d/service-${name}.cfg":
-         ensure => $ensure,
-         owner  => 'nagios',
-         group  => 'nagios',
-         tag    => "nagios_monitored_${instance_name}", 
+	 @@file { "/etc/nagios3/conf.d/{$host_name}/service-${name}.cfg":
+		ensure  => $ensure,
+		owner   => 'nagios',
+		group   => 'nagios',
+		content => template("nagios/nagios-service.erb")
+		tag     => "nagios_monitored_${instance_name}", 
      }
 }
