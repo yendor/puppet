51da97c75ea4b566da5b8215c0185bab14746e58
rodney.amato <rodney.amato@interspire.com>
2011-05-19 21:13:09 +1000

    Added nagios node

--- manifests/nodes/puppet.virtual.dojo.pp
+++ manifests/nodes/puppet.virtual.dojo.pp
@@ -1,58 +1,65 @@
 node "puppet.virtual.dojo" {
-  $mirror="http://ftp.au.debian.org/debian"
-  $storedconfig_db_user = "puppet"
-  $storedconfig_db_pass = "J2RL;wxG"
-  $storedconfig_db_name = "puppet"
-  $storedconfig_db_host = "localhost"
+	 $mirror="http://ftp.au.debian.org/debian"
+	 $storedconfig_db_user = "puppet"
+	 $storedconfig_db_pass = "J2RL;wxG"
+	 $storedconfig_db_name = "puppet"
+	 $storedconfig_db_host = "localhost"
 
-  include common
-  include git
-  include tinydns::setup
-  include puppetmaster
+	 include common
+	 include git
+	 include tinydns::setup
+	 include puppetmaster
 
-  dnsrecord { "nameserver for thdojo":
-    ensure => present,
-    type => ".",
-    fqdn => "virtual.dojo",
-    ipaddr => "192.168.1.15",
-    notify => Exec["rebuild-tinydns-data"]
-  }
+	dnsrecord { "nameserver for thdojo":
+		ensure => present,
+		type => ".",
+		fqdn => "virtual.dojo",
+		ipaddr => "192.168.1.15",
+		notify => Exec["rebuild-tinydns-data"]
+	}
 
-  dnsrecord { "reverse dns for 192.168.1.0/24 subnet":
-    ensure => present,
-    type => ".",
-    fqdn => "1.168.192.in-addr.arpa",
-    ipaddr => "192.168.1.15",
-    notify => Exec["rebuild-tinydns-data"]
-  }
+	dnsrecord { "reverse dns for 192.168.1.0/24 subnet":
+		ensure => present,
+		type => ".",
+		fqdn => "1.168.192.in-addr.arpa",
+		ipaddr => "192.168.1.15",
+		notify => Exec["rebuild-tinydns-data"]
+	}
 
-  dnsrecord { "alias for login.virtual.dojo":
-    ensure => present,
-    type => "+",
-    fqdn => "login.virtual.dojo",
-    ipaddr => "192.168.1.12",
-    notify => Exec["rebuild-tinydns-data"]
-  }
+	dnsrecord { "alias for login.virtual.dojo":
+		ensure => present,
+		type => "+",
+		fqdn => "login.virtual.dojo",
+		ipaddr => "192.168.1.12",
+		notify => Exec["rebuild-tinydns-data"]
+	}
 
-  dnsrecord { "alias for the puppet.virtual.dojo dns entry":
-    ensure => present,
-    type => "+",
-    fqdn => "puppet.virtual.dojo",
-    ipaddr => "192.168.1.15",
-    notify => Exec["rebuild-tinydns-data"]
-  }
+	dnsrecord { "alias for the puppet.virtual.dojo dns entry":
+		ensure => present,
+		type => "+",
+		fqdn => "puppet.virtual.dojo",
+		ipaddr => "192.168.1.15",
+		notify => Exec["rebuild-tinydns-data"]
+	}
 
-  dnsrecord { "txt record for virtual.dojo":
-    ensure => present,
-    fqdn => "virtual.dojo",
-    type => "'",
-    value => "v=spf1 a mx",
-    notify => Exec["rebuild-tinydns-data"]
-  }
+	dnsrecord { "txt record for virtual.dojo":
+		ensure => present,
+		fqdn => "virtual.dojo",
+		type => "'",
+		value => "v=spf1 a mx",
+		notify => Exec["rebuild-tinydns-data"]
+	}
 
-  class { "ssh-monitoring":
-	instance_name => "home",
-  }
+	class { "nagios::node":
+		address => $ipaddress,
+		host_name = $hostname,
+		alias => $fqdn,
+		contact_groups => "admins"
+	}
 
-  Dnsrecord <<| |>>
+	class { "ssh-monitoring":
+		instance_name => "home",
+	}
+
+	Dnsrecord <<| |>>
 }
--- modules/nagios/manifests/node.pp
+++ modules/nagios/manifests/node.pp
@@ -1,3 +1,27 @@
-class nagios::node {
-	Class["nagios::server"] -> Class["nagios::common"]
+class nagios::node($instance_name, 
+	$host_name,
+	$address,
+	$host_name,
+	$alias,	
+	$hostgroups,
+	$contact_groups,	
+	$check_command="check-host-alive",	
+	$max_check_attempts=4,
+	$notification_options="d,r",
+	$nottification_period="24x7",
+	$notification_interval=120,
+	$use="generic-host",
+
+) {
+	
+	@@file { "/etc/nagios3/conf.d/$host_name"
+		ensure => directory,
+		tag => $instance_name,
+	}
+	
+	@@file { "/etc/nagios3/conf.d/$host_name/host.cfg":
+		content => template("templates/nagios-node.erb"),
+		tag => $instance_name,
+	}
+	
 }
