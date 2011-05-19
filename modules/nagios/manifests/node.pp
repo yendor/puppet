36a2992dd031e96f2d2f6bae43e4adea2dc67b87
rodney.amato <rodney.amato@interspire.com>
2011-05-19 21:31:15 +1000

    Fix the path to the template

--- modules/nagios/manifests/node.pp
+++ modules/nagios/manifests/node.pp
@@ -1,28 +1,28 @@
-class nagios::node($instance_name, 
+class nagios::node($instance_name,
 	$host_name,
 	$address,
 	$host_name,
-	$host_alias,	
+	$host_alias,
 	$contact_groups,
-	$hostgroups="",	
-	$check_command="check-host-alive",	
+	$hostgroups="",
+	$check_command="check-host-alive",
 	$max_check_attempts=4,
 	$notification_options="d,r",
 	$nottification_period="24x7",
 	$notification_interval=120,
 	$use="generic-host"
 ) {
-	
+
 	@@file { "/etc/nagios3/conf.d/$host_name":
 		ensure => directory,
 		tag => "nagios_monitored_${instance_name}",
-		notify => Service["nagios"].	
+		notify => Service["nagios"],
 	}
-	
+
 	@@file { "/etc/nagios3/conf.d/$host_name/host.cfg":
 		content => template("nagios/nagios-node.erb"),
 		tag => "nagios_monitored_${instance_name}",
-		notify => Service["nagios"].
+		notify => Service["nagios"],
 	}
-	
+
 }
