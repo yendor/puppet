2aecb26bab9b5cd2fa341890092b3613b97814a7
rodney.amato <rodney.amato@interspire.com>
2011-05-21 00:33:31 +1000

    Fix up the ensure passthrough

--- manifests/templates/ssh-monitoring.pp
+++ manifests/templates/ssh-monitoring.pp
@@ -1,4 +1,4 @@
-class ssh-monitoring($instance_name, $host_name, $ensure => "present", $service_groups='')
+class ssh-monitoring($instance_name, $host_name, $ensure => 'present', $service_groups='')
 {
 	include nagios::common
 	
