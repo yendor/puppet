96d224d287f172169c3525c9a1ad896fa2614abd
rodney.amato <rodney.amato@interspire.com>
2011-05-20 23:06:40 +1000

    Added a mail server stub template

---
+++ manifests/templates/mail-server.pp
@@ -0,0 +1,25 @@
+class web-monitoring($instance_name, 
+	$service_groups='',
+	$smtp=true,
+	$submission=true,
+	$ssmtp=true,
+	$pop=true,
+	$imap=true,
+	$pops=true,
+	$imaps=true,
+)
+{
+	include nagios::common
+	
+	if ($smtp) {
+		nagios::service { "check_smtp":
+			ensure              => present, 
+			host_name           => $host_name, 
+			service_description => "SMTP",
+			check_command       => "check_smtp", 
+			instance_name       => $instance_name, 
+			servicegroups       => $service_groups,
+		}
+	}
+	
+}
