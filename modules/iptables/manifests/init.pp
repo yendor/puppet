82b8eec3186fa999cb2fe89dbb8d9b1964b7f466
rodney.amato <rodney.amato@interspire.com>
2011-06-07 11:44:48 +1000

    Add the option of including colordiff

--- modules/iptables/manifests/init.pp
+++ modules/iptables/manifests/init.pp
@@ -1,7 +1,8 @@
-class iptables {
+class iptables($include_diff=true) {
     package { "iptables":
 		ensure => present
 	}
+	
     
     file { "/etc/network/if-pre-up.d/iptables-pre-up": 
         source => "puppet:///modules/iptables/iptables-pre-up", 
@@ -10,15 +11,21 @@ class iptables {
 		group   => root,
 	    backup => false,
     }
+
+    if ($include_diff) {
+        package { "colordiff":
+    	    ensure => present
+    	}
     
-    file { "/usr/local/sbin/fwdiff.sh":
-		source  => "puppet:///modules/iptables/fwdiff.sh",    
-		owner   => root,
-		group   => root,
-		mode    => 0700,
-		backup  => false,
-		require => Package["colordiff"],
-	}
+        file { "/usr/local/sbin/fwdiff.sh":
+    		source  => "puppet:///modules/iptables/fwdiff.sh",    
+    		owner   => root,
+    		group   => root,
+    		mode    => 0700,
+    		backup  => false,
+    		require => Package["colordiff"],
+    	}
+    }
 	
 	file { "/etc/firewall":
 	    ensure => directory,
