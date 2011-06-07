218780bf79df11b75e5a3dafd6b29363fa15e194
rodney.amato <rodney.amato@interspire.com>
2011-06-07 11:36:11 +1000

    Testing custom chains

--- manifests/nodes/test.virtual.dojo.pp
+++ manifests/nodes/test.virtual.dojo.pp
@@ -16,17 +16,17 @@ node "test.virtual.dojo" {
 	class { "internal-nagios-server":
 		instance_name => "home"
 	}
-	
+
 	class { "web-monitoring":
 		instance_name => "home",
 		host_name => $fqdn,
 	}
-	
+
 	class { "ssh-monitoring":
 		instance_name => "home",
 		host_name => $fqdn,
 	}
-	
+
 	class { "nagios3::nrpe":
 		bind_to_ip => $ipaddress,
 		allow_from => "192.168.1.41",
@@ -44,7 +44,35 @@ node "test.virtual.dojo" {
 	#     context => "/files/etc/fstab",
 	#     changes => "set *[file = '/']/opt errors=remount-ro,noatime,nodiratime",
 	# }
-	
+
+	iptables { "115 create ratelimited custom chain":
+        customchain => "RATELIMITED",
+        table => "filter",
+    }
+
+    iptables { "zzzz - 1 Set a max rate limit to 30 pps average":
+        raw_rule => "-A RATELIMITED -m limit --limit 30/sec --limit-burst 6 -j RETURN",
+        table => "filter",
+    }
+    iptables { "zzzzz - 2 Drop traffic that exceeds the rate limit":
+        raw_rule => "-A RATELIMITED -j DROP",
+        table => "filter"
+    }
+
+    iptables { "115 create ratelimited custom chain":
+        customchain => "AARDVARK",
+        table => "filter",
+    }
+
+    iptables { "zzzz - 1 Set a max rate limit to 30 pps average":
+        raw_rule => "-A AARDVARK -m limit --limit 30/sec --limit-burst 6 -j RETURN",
+        table => "filter",
+    }
+    iptables { "zzzzz - 2 Drop traffic that exceeds the rate limit":
+        raw_rule => "-A AARDVARK -j DROP",
+        table => "filter"
+    }
+
 	class { "drobo-monitoring": }
 
 }
