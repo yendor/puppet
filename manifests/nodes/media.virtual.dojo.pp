2f87737e385c093071ab3a5bf71694d0e444b461
rodney.amato <rodney.amato@interspire.com>
2011-08-12 10:08:59 +1000

    Added some configuration required for airplay

--- manifests/nodes/media.virtual.dojo.pp
+++ manifests/nodes/media.virtual.dojo.pp
@@ -5,4 +5,65 @@ node "media.virtual.dojo" {
     package { "smbclient":
         ensure => installed,
     }
+    
+    package { "bzip2":
+        ensure => installed,
+    }
+    
+    package { "make":
+        ensure => installed,
+    }
+    
+    package { "checkinstall":
+        ensure => installed,
+    }
+    
+    package { "yasm":
+        ensure => installed,
+    }
+    
+    package { "git-core":
+        ensure => installed,
+    }
+    
+    package { "x264":
+        ensure => absent,
+    }
+    
+    file { "/etc/apt/sources.list.d/multimedia.list":
+        ensure  => file,
+        content => "deb http://debian.netcologne.de/debian-multimedia.org stable main",
+        owner   => root,
+        group   => root,
+        mode    => 0644,
+        notify  => Exec["update-packgelist"]
+    }
+    
+    package { "libx264-dev":
+        ensure => installed,
+        require => File["/etc/apt/sources.list.d/multimedia.list"]
+    }
+    
+    package { "libmp3lame-dev":
+        ensure => installed,
+        require => File["/etc/apt/sources.list.d/multimedia.list"]
+    }
+    
+    package { "libfaad-dev":
+        ensure => installed,
+        require => File["/etc/apt/sources.list.d/multimedia.list"]
+    }
+    
+    package { "mpeg4ip-server":
+        ensure => installed,
+        require => File["/etc/apt/sources.list.d/multimedia.list"]
+    }
+    
+    package { "pkg-config":
+        ensure => installed,
+    }
+    
+    package { "openjdk-6-jdk":
+        ensure => installed,
+    }
 }
