23fab75a713ced3fb5b90e028940a86ca654a186
Rodney Amato <rodnet@gmail.com>
2011-11-15 15:26:55 +1100

    Realise the new key from my work workstation

--- manifests/classes/unixadmins.pp
+++ manifests/classes/unixadmins.pp
@@ -6,6 +6,7 @@ class user::unixadmins inherits user::virtual {
 		Ssh_authorized_key["rodney-windows-home"],
 		Ssh_authorized_key["rodney-macbook"],
 		Ssh_authorized_key["rodney-testkey"],
+		Ssh_authorized_key["rodney-work"],
 		User["chris.boulton"],
 	)
 
