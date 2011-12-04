bfd0dad3ae2213e3edfbe6cb25a861d946181e88
Rodney Amato <rodney.amato@brennanit.com.au>
2011-12-05 08:46:48 +1100

    Disable capabilities on openvze guests to avoid getting some errors with "Error setting capabilities, capability management disabled; error=Operation not permitted"

--- manifests/classes/syslog-ng.pp
+++ manifests/classes/syslog-ng.pp
@@ -2,4 +2,17 @@ class syslog-ng {
   package { "syslog-ng":
     ensure => present
   }
+
+  service { "syslog-ng":
+  	ensure => running,
+  	hastatus => false,
+  	hasrestart => true,
+  }
+
+  if ($virtual == "openvzve") {
+  	file { "/etc/default/syslog-ng":
+  	  source => "puppet:///files/syslog-ng/default-openvze",
+  	  notify => Service["syslog-ng"]
+  	}
+  }
 }
