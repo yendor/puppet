aec6d116f5721ebcfbc499f9466d4349c93a967a
Rodney Amato <rodnet@gmail.com>
2011-12-08 12:45:38 +1100

    Add a git user for gitolite

--- manifests/classes/git-server.pp
+++ manifests/classes/git-server.pp
@@ -2,4 +2,21 @@ class git-server {
 	package { "gitolite":
 		ensure => present
 	}
+
+	group { "git":
+		ensure => present,
+		name => "git",
+		gid => 201
+	}
+
+	user { "git":
+		ensure => present,
+		name => "git",
+		comment => "Gitolite User",
+		home => "/home/git",
+		shell => "/bin/bash",
+		system => true,
+		uid => 201,
+		gid => "git",
+	}
 }
