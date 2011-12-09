5a325f8d982cc4975a565aa7bebb14d58f2f3da9
Rodney Amato <rodnet@gmail.com>
2011-12-09 13:17:18 +1100

    Fix the target for the ssh key

--- manifests/classes/git-server.pp
+++ manifests/classes/git-server.pp
@@ -42,7 +42,7 @@ class git-server {
 		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCu2AwOz+TTGgehU9FXtNjq5vR1l8dXSlTLp44c9Ce/UdF+yyx2S5435Tcw/2EcuQXTEwAr+VjXla/f/2O1UEvFbgTcTt/VNd1oFVueZP0OTAA5HGUpMJNmTbozrGMUIPMM3Ew6NDIpGdJZGjdxn2/ZYg4PHEQqnrSCXM7ZhKKJ1qzrothh84Tna/3eDwMg9qnqaso7d4P29a7YMya+XzDwB0HPk4eWXfSwRvNR+CxSiGMlalY4tDldW7xc9C0KDth1sMG4KjQvc6LlpF2T+P8YismcVAI+YF8jyakjrkhYZyl2GOzM0Wif8/6hTIxRp/XocB3/CrsYdzoevLulSllR",
 		type => "ssh-rsa",
 		user => "git",
-		target => "/home/git/seed.key",
+		target => "/home/git/seed.pub",
 		require => File["/home/git"]
 	}
 
