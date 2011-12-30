3bd7f92a7a71769bf40acee183d09979b95016a7
Rodney Amato <rodnet@gmail.com>
2011-12-30 14:22:29 +1100

    Fix lint violations

--- manifests/nodes/leech.virtual.dojo.pp
+++ manifests/nodes/leech.virtual.dojo.pp
@@ -1,28 +1,28 @@
-node "leech.virtual.dojo" {
-  $mirror="http://ftp.au.debian.org/debian"
+node 'leech.virtual.dojo' {
+  $mirror='http://ftp.au.debian.org/debian'
   include common
 
   nagios3::host { $fqdn:
-    instance_name => "home",
-    address => $ipaddress,
-    host_name => $fqdn,
-    host_alias => $hostname,
-    contact_groups => "admins"
+    instance_name  => 'home',
+    address        => $ipaddress,
+    host_name      => $fqdn,
+    host_alias     => $hostname,
+    contact_groups => 'admins'
   }
 
-  class { "ssh-monitoring":
-    instance_name => "home",
-    host_name => $fqdn,
-  }
-  
-  class { "nagios3::nrpe":
-    bind_to_ip => $ipaddress,
-    allow_from => "192.168.1.41",
-    instance_name => "home"
+  class { 'ssh-monitoring':
+    instance_name => 'home',
+    host_name     => $::fqdn,
   }
 
-  # augeas { "root_partition_noatime":
-  #     context => "/files/etc/fstab",
+  class { 'nagios3::nrpe':
+    bind_to_ip    => $::ipaddress,
+    allow_from    => '192.168.1.41',
+    instance_name => 'home'
+  }
+
+  # augeas { 'root_partition_noatime':
+  #     context => '/files/etc/fstab',
   #     changes => "set *[file = '/']/opt errors=remount-ro,noatime,nodiratime",
   # }
 }
