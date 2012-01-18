a1ba2872b4bbbb2a3847cdb494ea22acd71e5a29
Rodney Amato <rodnet@gmail.com>
2012-01-19 08:43:55 +1100

    Remove portmap from the soe. Also reindent the file for puppet-lint compliance

--- manifests/classes/common.pp
+++ manifests/classes/common.pp
@@ -1,15 +1,15 @@
 class common {
-    include vim, bash, ssh-server, less, rsync, apticron, logcheck, mail::aliases, clear-apt-cache, avahi, sudo, htop, bzip2
-    include puppet
-    include postfix::standard
-    include git
+  include vim, bash, ssh-server, less, rsync, apticron, logcheck, mail::aliases, clear-apt-cache, avahi, sudo, htop, bzip2
+  include puppet
+  include postfix::standard
+  include git
 
-    include rodney-home
+  include rodney-home
 
-    $includeBackports=true
-    include sources-list, syslog-ng
+  $includeBackports=true
+  include sources-list, syslog-ng
 
-    include user::unixadmins
+  include user::unixadmins
 
   package { [
     "debian-goodies",
@@ -24,30 +24,34 @@ class common {
     backup => false
   }
 
-    # @@dnsrecord { "forward and reverse dns for $fqdn":
-    #     ensure => "present",
-    #     type   => "=",
-    #     fqdn   => "$fqdn",
-    #     ipaddr => "$ipaddress",
-    #     ttl    => 300,
-    #     notify => Exec["rebuild-tinydns-data"]
-    # }
+  package { "portmap":
+	ensure absent
+  }
 
-    file { "/etc/network/if-up.d/mountnfs":
-        mode => 0755
-    }
+  # @@dnsrecord { "forward and reverse dns for $fqdn":
+  #     ensure => "present",
+  #     type   => "=",
+  #     fqdn   => "$fqdn",
+  #     ipaddr => "$ipaddress",
+  #     ttl    => 300,
+  #     notify => Exec["rebuild-tinydns-data"]
+  # }
 
-    augeas{ "boot_delay":
-        context => $lsbdistcodename ? {
+  file { "/etc/network/if-up.d/mountnfs":
+	mode => 0755
+  }
+
+  augeas{ "boot_delay":
+	context => $lsbdistcodename ? {
       lenny   => "/files/boot/grub/menu.lst",
       squeeze => "/files/etc/default/grub",
-    },
-        changes => $lsbdistcodename ? {
+	},
+	changes => $lsbdistcodename ? {
       lenny   => "set timeout 5",
       squeeze => "set GRUB_TIMEOUT 5",
     },
     require => Package["lsb-release"],
-    }
+  }
 
   if ($operatingsystem == "Debian" and $includeBackports) {
     file { "/etc/apt/preferences":
