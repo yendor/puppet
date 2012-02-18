bc5be51117ad73de64a53673ec08f7bfa80f8012
Rodney Amato <rodnet@gmail.com>
2012-02-18 20:19:34 +1100

    Add base node definition

---
+++ manifests/nodes/ci.virtual.dojo.pp
@@ -0,0 +1,5 @@
+node 'ci.virtual.dojo' {
+  $mirror='http://ftp.au.debian.org/debian'
+  $includeBackports=true
+  include common
+}
---
+++ manifests/nodes/graphite.virtual.dojo.pp
@@ -0,0 +1,5 @@
+node 'graphite.virtual.dojo' {
+  $mirror='http://ftp.au.debian.org/debian'
+  $includeBackports=true
+  include common
+}
