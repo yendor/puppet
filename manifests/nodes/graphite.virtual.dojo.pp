9d5cea6db4fd96662039aad4c541f09073c5fa21
Rodney Amato <rodnet@gmail.com>
2012-02-19 11:05:35 +1100

    Pad out the graphite server requirements a bit

--- manifests/nodes/graphite.virtual.dojo.pp
+++ manifests/nodes/graphite.virtual.dojo.pp
@@ -6,4 +6,23 @@ node 'graphite.virtual.dojo' {
   package { 'bzr':
     ensure => present
   }
+
+  package { 'memcached':
+    ensure => present,
+    enable => true,
+  }
+  service { 'memcached':
+    ensure => running,
+  }
+  include apache2
+
+  package { [
+    'python-django',
+    'python-django-tagging',
+    'python-memcache',
+    'python-twisted',
+    'python-zope.interface'
+    ]:
+    ensure => present
+  }
 }
