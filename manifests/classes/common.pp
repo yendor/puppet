5af64d161145bab7f6c7f83d378351e7ca615194
Rodney Amato <rodnet@gmail.com>
2012-03-30 13:34:35 +1100

    Add support for ubuntu oneric

--- manifests/classes/common.pp
+++ manifests/classes/common.pp
@@ -46,10 +46,12 @@ class common {
 	context => $lsbdistcodename ? {
       lenny   => "/files/boot/grub/menu.lst",
       squeeze => "/files/etc/default/grub",
+      oneric => "/files/etc/default/grub",
 	},
 	changes => $lsbdistcodename ? {
       lenny   => "set timeout 5",
       squeeze => "set GRUB_TIMEOUT 5",
+      oneric => "set GRUB_TIMEOUT 5",
     },
     require => Package["lsb-release"],
   }
